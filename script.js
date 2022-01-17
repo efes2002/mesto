7a1f3caa3274383b39bb12934e543fbf47215bf7
Zhidkov Kirill <kirill-info@yandex.ru>
2022-01-18 02:05:27 +0300

    Исправление ошибок после первой проверки ПР

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 let formElement = document.querySelector('.popup__form');
 let buttonEdit = document.querySelector('.profile__edit-button');
 let buttonX = document.querySelector('.popup__button-x');
-
+let popupElement = document.querySelector('.popup');
 let nameElement = document.querySelector('.profile__name');
 let jobElement = document.querySelector('.profile__job');
-
 let nameInput = document.querySelector('#nameInput');
 let jobInput = document.querySelector('#jobInput');
 
-// При запуске страницы устанавливаем плейсхолдеры для формы отправки
-nameInput.setAttribute('placeholder', nameElement.textContent);
-jobInput.setAttribute('placeholder', jobElement.textContent);
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  let newName = nameInput.value;
-  let newJob = jobInput.value;
-
-  if ((typeof newName === 'string') && (newName.length > 0)) {
-    nameElement.textContent = newName;
-    nameInput.setAttribute('placeholder', newName);
-  }
-  if ((typeof newJob === 'string') && (newJob.length > 0)) {
-    jobElement.textContent = newJob;
-    jobInput.setAttribute('placeholder', newJob);
-  }
-
+  nameElement.textContent = evt.target.name.value;
+  jobElement.textContent = evt.target.job.value;
   togglePopUp();
 }
 
 function togglePopUp() {
-  document.querySelector('.popup').classList.toggle('popup_opened');
+  popupElement.classList.toggle('popup_opened');
+}
+
+function openPopUp() {
+  nameInput.setAttribute('value', nameElement.textContent);
+  jobInput.setAttribute('value', jobElement.textContent);
+  togglePopUp();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-buttonEdit.addEventListener('click', togglePopUp);
+buttonEdit.addEventListener('click', openPopUp);
 buttonX.addEventListener('click', togglePopUp);
 
